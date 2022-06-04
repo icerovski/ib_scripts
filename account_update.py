f523f15d95e2ce9af05f2241fe3ae9d64a900be2
icerovski <ivan.cerovski@gmail.com>
2022-06-04 15:16:57 +0300

    identify only realized transactions

--- account_update.py
+++ account_update.py
@@ -1,5 +1,5 @@
 import csv
-from operator import sub
+from typing import Type
 
 def call_data(source_file, main_criterion, sub_criteria):
     data = []
@@ -14,87 +14,10 @@ def call_data(source_file, main_criterion, sub_criteria):
                     assigned_header = True
                     continue
 
-                if row[1] == sub_criteria['Header'] and \
-                row[2] in sub_criteria['DataDiscriminator']:
+                if row[1] == sub_criteria['Header'] and row[2] in sub_criteria['DataDiscriminator']:
                     data.append(row)
 
-    # counter = 0
-    # with open(str(source_file), "r") as source_file:
-    #     reader = csv.reader(source_file, delimiter=",")
-    #     for row in reader:
-    #         if row[0] == "Trades" and (row[1] == "Data" or row[1] == "ClosedLot"):
-    #             counter += 1  # used to count the transactions
-    #             row = [
-    #                 counter,
-    #                 *row,
-    #             ]  # using unpack insert an item in the begining of the list
-    #             data.append(row)  # append the newly created row to the data
-    #             print(data)
-
     return data
-
-def pull_ticker(row, ticker_index):
-    ticker_name = row[ticker_index]
-    if ticker_name and ticker_name != 'Symbol':
-        return ticker_name
-    else:
-        return None
-
-def call_ticker_names(data_list):
-    symbol_index = header_index('Symbol', data_list)
-    ticker_list = []
-    # Start from 1 to omit the Header row
-    for i in range(1, len(data_list)):
-        row = data_list[i]
-        ticker = row[symbol_index]
-        # Excludes 'Symbol' as a potential ticker name, because it catches it as such.
-        if ticker and ticker != 'Symbol':
-            if ticker not in ticker_list:
-                ticker_list.append(ticker)
-
-    return ticker_list
-
-def header_index(header, array):
-    return array[0].index(header)
-
-def pull_ticker_data(ticker, data_list):
-    symbol_index = header_index('Symbol', data_list)
-    ticker_raw_data = []
-    for i in range(len(data_list)):
-        if data_list[i][symbol_index] == ticker:
-            ticker_raw_data.append(data_list[i])
-
-    return ticker_raw_data
-
-def transponse(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    matrix_t = []
-    for width in range(cols):
-        row = []
-        for height in range(rows):
-            row.append(matrix[height][width])
-        matrix_t.append(row)
-
-    return matrix_t
-
-def compute_ticker_data(symbol, data):
-    result = pull_ticker_data(symbol, data)
-    return transponse(result)
-
-# def trade_entry = {
-#         "Ticker":str,
-#         "Date entry":str,
-#         "Quantity entry":int,
-#         "Price entry":float,
-#         "Expense":float,
-#         "Date exit":str,
-#         "Quantity exit":int,
-#         "Price exit":float,
-#         "Income":float,
-#         "Profit":float,
-#     }
 
 def main():
     source_file_name = "ib_statement.csv"
@@ -103,8 +26,9 @@ def main():
         'DataDiscriminator':('Trade', 'ClosedLot')
     }
     trades_data = call_data(source_file_name, 'Trades', trades_criteria)
-    
-    key_titles = (
+    headers = trades_data.pop(0)
+
+    key_headers_list = (
         'Symbol',
         'Asset Category',
         'Currency',
@@ -114,66 +38,52 @@ def main():
         'Quantity',
         'T. Price'
     )
-    headers = {}
-    print(trades_data[0])
-    print(trades_data[0].index('Header'))
-    for item in trades_data[0]:
-        if item in (key_titles):
-            headers[item] = trades_data[0].index(item)
+    header_index = {}
+    for item in headers:
+        if item in (key_headers_list):
+            header_index[item] = headers.index(item)
 
-    print(headers.items())
-
-    # ticker_index = header_index('Symbol', trades_data)
-    # asset_category_index = header_index('Asset Category', trades_data)
-    # currency_index = header_index('Currency', trades_data)
-    # exchange_index = header_index('Exchange', trades_data)
-    # transaction_type_index = header_index('DataDiscriminator', trades_data)
-    # date_index = header_index('Date/Time', trades_data)
-    # quantity_index = header_index('Quantity', trades_data)
-    # price_index = header_index('T. Price', trades_data)
-
-
-    ticker_dict = {}
+    tickers_data = {}
     for row in trades_data:
-        ticker_name = pull_ticker(row, headers['Symbol'])
-        if ticker_name:
-            ticker_current_transactions = {
-                'Transaction': row[headers['DataDiscriminator']],
-                'Date':row[headers['Date/Time']],
-                'Quantity':row[headers['Quantity']],
-                'Price':row[headers['T. Price']]
-            }
-            if ticker_name not in ticker_dict:
-                ticker_dict[ticker_name] = {}
-                ticker_dict[ticker_name]['Asset Category'] = row[headers['Asset Category']]
-                ticker_dict[ticker_name]['Currency'] = row[headers['Currency']]
-                ticker_dict[ticker_name]['Exchange'] = row[headers['Exchange']]
-                ticker_dict[ticker_name]['Transactions'] = [ticker_current_transactions]
-            else:
-                ticker_dict[ticker_name]['Transactions'].append(ticker_current_transactions)
+        ticker_name = row[header_index['Symbol']]
+        ticker_current_transactions = {
+            'Type': row[header_index['DataDiscriminator']],
+            'Date':row[header_index['Date/Time']],
+            'Quantity':row[header_index['Quantity']],
+            'Price':row[header_index['T. Price']]
+        }
+        if ticker_name not in tickers_data:
+            tickers_data[ticker_name] = {}
+            tickers_data[ticker_name]['Asset Category'] = row[header_index['Asset Category']]
+            tickers_data[ticker_name]['Currency'] = row[header_index['Currency']]
+            tickers_data[ticker_name]['Exchange'] = row[header_index['Exchange']]
+            tickers_data[ticker_name]['Transactions'] = [ticker_current_transactions]
+        else:
+            tickers_data[ticker_name]['Transactions'].append(ticker_current_transactions)
 
-    print(ticker_dict.keys())
-    print(ticker_dict.values())
-    print(ticker_dict.items())
+    # for key, value in tickers_data.items():
+    #     print(key, value)
+    # for key in tickers_data:
+    #     current_ticker = tickers_data[key]
+    #     cat = current_ticker['Asset Category']
+    #     realized = current_ticker['Transactions']
+    #     rlzd_value = 'ClosedLot'
+    #     list_of_bool = [True for elem in realized if rlzd_value in elem.values()]
+    #     if any(list_of_bool):
+    #         print(f'{key} - {cat}')
 
-    tickers = call_ticker_names(trades_data)
-
-    for ticker in tickers:
-        ticker_data = compute_ticker_data(ticker, trades_data)
-        profit_index = header_index('Realized P/L', trades_data)
-        flag_index = header_index('DataDiscriminator', trades_data)
-
-        for i in range(len(ticker_data[0])):
-            # If trade has zero realized Profit or Loss, this means it is not yet closed.
-            # So, you skip to next 'i', because you only report realized profit/ loss.
-            rlzd = ticker_data[profit_index][i]
-            if float(rlzd) != 0:
-                flag = ticker_data[flag_index][i]
-                if flag in ('Trade', 'ClosedLot'):
-                    print(f'{ticker}: {ticker_data[flag_index][i]}')
-
-        for i, v in enumerate(ticker_data[flag_index]):
-            if v == 'Trade':
-                print(i, v)
+    rlzd_value = 'ClosedLot'
+    for key in tickers_data:    
+        ticker_transactions = tickers_data[key]['Transactions']
+        is_realized = False
+        for i in ticker_transactions:
+            if i['Type'] == rlzd_value:
+                is_realized = True
+                break
+        if is_realized:
+            print(f'{key} has realized profit')
+        else:
+            continue
+        
 if __name__ == "__main__":
     main()
